90ff9256a800951228f8e2d734c9f6383f7b980b
Harkame <louisr.daviaud@gmail.com>
2023-09-12 17:21:24 +0200

    Update __main__.py

--- google_images_downloader/__main__.py
+++ google_images_downloader/__main__.py
@@ -1,18 +1,17 @@
 import sys
+
 import google_images_downloader
 
-from google_images_downloader import GoogleImagesDownloader
-from helpers.arguments_helper import get_arguments
 
-
-def main():
-    arguments = get_arguments(sys.argv[1:])
+def main(argv):
+    arguments = google_images_downloader.get_arguments(argv)
 
     google_images_downloader.WEBDRIVER_WAIT_DURATION = arguments.wait_duration
 
-    downloader = GoogleImagesDownloader(browser=arguments.browser, show=arguments.show, debug=arguments.debug,
-                                        quiet=arguments.quiet,
-                                        disable_safeui=arguments.disable_safeui)
+    downloader = google_images_downloader.GoogleImagesDownloader(browser=arguments.browser, show=arguments.show,
+                                                                 debug=arguments.debug,
+                                                                 quiet=arguments.quiet,
+                                                                 disable_safeui=arguments.disable_safeui)
 
     for query in arguments.queries:
         downloader.download(query, destination=arguments.destination, limit=arguments.limit, resize=arguments.resize,
@@ -22,4 +21,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1:])
